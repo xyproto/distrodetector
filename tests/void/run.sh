9f365f5040bf4ef77c748166e3f09b83f6179913
Alexander F. Rødseth <rodseth@gmail.com>
2018-11-06 23:50:47 -0800

    Define an environment variable on the top level of the CI tests

--- tests/void/run.sh
+++ tests/void/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scriptdir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$scriptdir"
-docker build --no-cache -t d4:void . && docker run --rm --name d4_void d4:void
+docker build --no-cache -t d4:void . && docker run -e DISTRODETECT --rm --name d4_void d4:void
