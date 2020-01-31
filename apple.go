18b5e9b532da3c8601fd8e3667394e0a7e63366e
Alexander F. Rødseth <rodseth@gmail.com>
2020-01-31 15:20:06 +0100

    Add Catalina

--- apple.go
+++ apple.go
@@ -33,6 +33,7 @@ func AppleCodename(version string) string {
 		"10.12": "Sierra",
 		"10.13": "High Sierra",
 		"10.14": "Mojave",
+		"10.15": "Catalina",
 	}
 	// Search the keys, longest keys first
 	for keyLength := 5; keyLength >= 4; keyLength-- {
